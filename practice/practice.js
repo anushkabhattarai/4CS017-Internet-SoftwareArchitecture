05e9e2bc62bc8a0b95be7190e50a576669be4ce4
anushkabhattarai <np03cs4a220241@heraldcollege.edu.np>
2023-03-30 20:03:59 +0545

    learning basics of javascript

---
+++ practice/practice.js
@@ -0,0 +1,24 @@
+// create a variable of the type string and try to add a number to it
+let a="anushka"
+let b=6
+console.log(a+b)
+//  use typeof operato)rs to find the datatype of the string in the last question
+console.log(typeof (a+b))
+// create a const object in javascript. can you chnage it to hold a number later?
+const and={
+    name:"anushka",
+    section:1,
+}
+// try to add a new key to the const obejct in problem 3. were you able to do it?
+and['friend']="amanda"
+and['name']="anu"
+console.log(and)
+// write a js program to crate a word meaning dictionary of 4 words
+const dict ={
+    appreciate:"recognize the full worth",
+    today:"i am feeling happy",
+    mind:"my head is roaming",
+    yaka:"work, especially hard work"
+}
+console.log(dict.yaka) // or console.log(dict['yaka])
+
